8c4286906448c5c5faf494ef7089f92f1770788f
Tobias Heilig <0x70b1@web.de>
2019-01-03 20:04:07 +0100

    Update hashmap.h

--- hashmap/hashmap.h
+++ hashmap/hashmap.h
@@ -77,7 +77,6 @@ extern int hm_remove (Hashmap hm, Key key);
 /* iterate the hashmap and call f for each key-value pair */
 extern int hm_iter (Hashmap hm, PFiter f);
 
-/* get the current count of bindings from the hasmap*/
-extern size_t hm_count (Hashmap hm);
-
+/* retreive the current count of bindings from the hasmap*/
+extern int hm_count (Hashmap hm, size_t *count);
 
